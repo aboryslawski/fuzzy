598e7a696941539cf00082bcc6b52d74e0db6ee2
aboryslawski <adamboryslawski97@gmail.com>
2023-05-14 17:11:44 +0200

    second commit

--- main.py
+++ main.py
@@ -5,56 +5,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 
-# Input variable: x
+# zmienna wejsciowa: x
 x = np.linspace(0, 10, 100)
 
-# Fuzzy membership functions for input variable x
-x_low = fuzz.trimf(x, [0, 0, 5])
-x_medium = fuzz.trimf(x, [0, 5, 10])
-x_high = fuzz.trimf(x, [5, 10, 10])
+# funkcje przynaleznosci fuzzy dla danej wejsciowej x
+inside_low = fuzz.trimf(x, [0, 0, 5])
+inside_medium = fuzz.trimf(x, [0, 5, 10])
+inside_high = fuzz.trimf(x, [5, 10, 10])
 
-# Output variable: y
+# zmienna wyjsciowa: y
 y = np.linspace(0, 20, 100)
 
-# Fuzzy membership functions for output variable y
-y_low = fuzz.trimf(y, [0, 0, 10])
-y_medium = fuzz.trimf(y, [0, 10, 20])
-y_high = fuzz.trimf(y, [10, 20, 20])
+# funkcje przynaleznosci fuzzy dla danej wyjsciowej y
+outside_low = fuzz.trimf(y, [0, 0, 10])
+outside_medium = fuzz.trimf(y, [0, 10, 20])
+outside_high = fuzz.trimf(y, [10, 20, 20])
 
-# Rule 1: IF x is low THEN y is low
-rule1 = np.fmin(x_low, y_low)
+# zasada 1: jesli x jest niskie THEN y is niskie
+rule1 = np.fmin(inside_low, outside_low)
 
-# Rule 2: IF x is medium THEN y is medium
-rule2 = np.fmin(x_medium, y_medium)
+# zasada 2: jesli x jest srednie THEN y is srednie
+rule2 = np.fmin(inside_medium, outside_medium)
 
-# Rule 3: IF x is high THEN y is high
-rule3 = np.fmin(x_high, y_high)
+# zasada 3: jesli x jest wysokie THEN y is wysokie
+rule3 = np.fmin(inside_high, outside_high)
 
-# Aggregate all output membership functions
+# agregacja wszystkich wyjsciowych funkcji przynaleznosci
 aggregated = np.fmax(rule1, np.fmax(rule2, rule3))
 
-# Defuzzify the aggregated membership function
+# defuzyfikacja zagregowanych funkcji przynaleznosci metoda centroidu
 output = fuzz.defuzz(y, aggregated, 'centroid')
 
-# Visualize the membership functions and the aggregated result
+# wizualizacja funkcji przynaleznosci oraz zagregowanych wynikow
 plt.figure(figsize=(8, 6))
 
-plt.plot(x, x_low, 'b', linewidth=1.5, label='x_low')
-plt.plot(x, x_medium, 'g', linewidth=1.5, label='x_medium')
-plt.plot(x, x_high, 'r', linewidth=1.5, label='x_high')
+plt.plot(x, inside_low, 'b', linewidth=1.5, label='inside_low')
+plt.plot(x, inside_medium, 'g', linewidth=1.5, label='inside_medium')
+plt.plot(x, inside_high, 'r', linewidth=1.5, label='inside_high')
 
-plt.plot(y, y_low, 'c', linewidth=1.5, label='y_low')
-plt.plot(y, y_medium, 'm', linewidth=1.5, label='y_medium')
-plt.plot(y, y_high, 'y', linewidth=1.5, label='y_high')
+plt.plot(y, outside_low, 'c', linewidth=1.5, label='outside_low')
+plt.plot(y, outside_medium, 'm', linewidth=1.5, label='outside_medium')
+plt.plot(y, outside_high, 'y', linewidth=1.5, label='outside_high')
 
 plt.plot(y, aggregated, 'k', linewidth=2, label='aggregated')
 
 plt.fill_between(y, 0, aggregated, facecolor='Orange', alpha=0.7)
 plt.vlines(output, 0, 1, colors='r', linestyle='dashed')
 
-plt.title('Takagi-Sugeno Fuzzy Logic System')
+plt.title('kontrola temperatury')
 plt.xlabel('Input (x) / Output (y)')
-plt.ylabel('Membership Degree')
+plt.ylabel('stopien przynaleznosci')
 plt.legend()
 plt.grid(True)
 plt.show()
