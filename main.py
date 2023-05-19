adec4aa376ec6766fd7efd9d3b35d0c91c61d1f7
aboryslawski <adamboryslawski97@gmail.com>
2023-05-19 22:17:38 +0200

    major amendments

--- main.py
+++ main.py
@@ -1,61 +1,55 @@
-# installing dependencies:
-# pip3 install -r requirements.txt
-
+from simpful import *
 import numpy as np
 import matplotlib.pyplot as plt
-import skfuzzy as fuzz
 
-# zmienna wejsciowa: x
-x = np.linspace(0, 15, 150)
+FS = FuzzySystem()
 
-# funkcje przynaleznosci fuzzy dla danej wejsciowej x
-quality_low = fuzz.trimf(x, [0, 0, 5])
-quality_medium = fuzz.trimf(x, [0, 5, 15])
-quality_high = fuzz.trimf(x, [5, 15, 15])
+TLV = AutoTriangle(3, terms=['poor', 'average', 'good'], universe_of_discourse=[0,10])
+FS.add_linguistic_variable("service", TLV)
+FS.add_linguistic_variable("quality", TLV)
 
-# zmienna wyjsciowa: y
-y = np.linspace(0, 20, 150)
+O1 = TriangleFuzzySet(0,0,13,   term="low")
+O2 = TriangleFuzzySet(0,13,25,  term="medium")
+O3 = TriangleFuzzySet(13,25,25, term="high")
+FS.add_linguistic_variable("tip", LinguisticVariable([O1, O2, O3], universe_of_discourse=[0,25]))
 
-# funkcje przynaleznosci fuzzy dla danej wyjsciowej y
-speed_low = fuzz.trimf(y, [0, 0, 15])
-speed_medium = fuzz.trimf(y, [0, 15, 20])
-speed_high = fuzz.trimf(y, [15, 20, 20])
+FS.add_rules([
+    "IF (quality IS poor) OR (service IS poor) THEN (tip IS low)",
+    "IF (service IS average) THEN (tip IS medium)",
+    "IF (quality IS good) OR (service IS good) THEN (tip IS high)"
+    ])
 
-# zasada 1: jesli x jest niskie to y is niskie
-rule1 = np.fmin(quality_low, speed_low)
+x = []
+y = []
+z = []
 
-# zasada 2: jesli x jest srednie to y is srednie
-rule2 = np.fmin(quality_medium, speed_medium)
+for a in range(10):
+    for b in range(10):
+        x.append(a)
+        y.append(b)
+        FS.set_variable("quality", a)
+        FS.set_variable("service", b)
+        tip = FS.inference()['tip']
+        z.append(tip)
 
-# zasada 3: jesli x jest wysokie to y is wysokie
-rule3 = np.fmin(quality_high, speed_high)
+X = x
+Y = y
+Z = z
 
-# agregacja wszystkich wyjsciowych funkcji przynaleznosci
-aggregated = np.fmax(rule1, np.fmax(rule2, rule3))
+x = np.reshape(X, (10, 10))
+y = np.reshape(Y, (10, 10))
+z = np.reshape(Z, (10, 10))
 
-# defuzyfikacja zagregowanych funkcji przynaleznosci metoda centroidu
-output = fuzz.defuzz(y, aggregated, 'centroid')
 
-# wizualizacja funkcji przynaleznosci oraz zagregowanych wynikow
-plt.figure(figsize=(8, 6))
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-plt.plot(x, quality_low, 'b', linewidth=1.5, label='quality_low')
-plt.plot(x, quality_medium, 'g', linewidth=1.5, label='quality_medium')
-plt.plot(x, quality_high, 'r', linewidth=1.5, label='quality_high')
+ax.plot_surface(x, y, z)
 
-plt.plot(y, speed_low, 'c', linewidth=1.5, label='speed_low')
-plt.plot(y, speed_medium, 'm', linewidth=1.5, label='speed_medium')
-plt.plot(y, speed_high, 'y', linewidth=1.5, label='speed_high')
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
 
-plt.plot(y, aggregated, 'k', linewidth=2, label='aggregated')
-
-plt.fill_between(y, 0, aggregated, facecolor='Orange', alpha=0.7)
-plt.vlines(output, 0, 1, colors='r', linestyle='dashed')
-
-plt.title('napiwek dla obslugi')
-plt.xlabel('procent rachunku')
-plt.ylabel('stopien przynaleznosci')
-plt.legend()
-plt.grid(True)
 plt.show()
+quit()
 
